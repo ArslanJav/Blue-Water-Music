240006677fcd4c2d86df8417c10e6aac6981d606
Arslan Javed <80915650+ArslanJav@users.noreply.github.com>
2023-10-23 23:59:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,17 @@ for (var i = 0; i < startOrStopImgs.length; i++) {
 }
 
 // function that takes startOrStopImg elements with corresponding audio elements and add the onclick event handler with the abilty to play and pause the audio and when another is clicked, the first one will stop and the second one will play and so on
-function addAudioPlayPauseEvent(startOrStopImg, audio, audios) {
+function addAudioPlayPauseEvent(startOrStopImg, audio) {
   startOrStopImg.onclick = function () {
-    // Pause all other audios
-    for (var i = 0; i < audios.length; i++) {
-      if (audios[i] !== audio) audios[i].pause();
-    }
-
-    // Toggle between play and pause
     if (audio.paused) {
-      // If audio is paused, set the currentTime to 22 seconds and play
-      audio.currentTime = 22;
       audio.play();
-    } else {
-      // If audio is playing, pause it
-      audio.pause();
-    }
+      for (var i = 0; i < audios.length; i++) {
+        if (audios[i] != audio) audios[i].pause();
+      }
+    } else audio.play();
+    // audio starts playing at 22 seconds
+    audio.currentTime = 22;
+    audio.paused();
   };
 }
 
